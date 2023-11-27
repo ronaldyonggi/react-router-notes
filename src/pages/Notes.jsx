1e589c88612ec072a1b74ed61a310f677866e4d1
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-27 15:49:55 +0800

    Notes page lists notes content and their Links

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,16 @@
-export default function Notes() {
+import { Link } from "react-router-dom";
+
+export default function Notes({ notes }) {
     return (
         <div>
             <h2>Notes</h2>
+            <ul>
+                {notes.map(note => 
+                <li key={note.id}>
+                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                </li>
+                    )}
+            </ul>
         </div>
     )
 }
