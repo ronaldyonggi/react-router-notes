0d55f8ab96ddbe28e0167af8cd54862a3f1f9a75
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-11-27 15:49:37 +0800

    add Users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,12 @@
 export default function Users() {
     return (
         <div>
-            <h2>Users</h2>
+            <h2>TKTL Notes App</h2>
+            <ul>
+                <li>Matti Luukkainen</li>
+                <li>Juha Tauriainen</li>
+                <li>Arto Hellas</li>
+            </ul>
         </div>
     )
 }
